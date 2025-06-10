e1a697bc508027a283202972c4ab7e089c3c9453
izhans <izhansastre@gmail.com>
2025-06-10 13:17:26 +0200

    solve segfault when freeing map

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 17:50:50 by isastre-          #+#    #+#             */
-/*   Updated: 2025/06/10 10:56:41 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/06/10 13:15:24 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,19 @@ void ft_print_error(char *error_msg);
 
 int main(int argc, char *argv[])
 {
-	t_map_data	*map = ft_calloc(1, sizeof(t_map_data));
+	t_map_data	*map;
 	
 	// check map arg exists + check if .ber
 	if (argc != 2 || !ft_file_is_dot_ber(argv[1]))
 		return (1);
 	
+	map = ft_calloc(1, sizeof(t_map_data));
 	if (ft_init_map(map, argv[1]))
-	{
-		ft_free_map_struct(map);
-		return (1);
-	}
+		return (ft_free_map_struct(map), 1);
 	printf("mapa bien\n");
 
 	// free map + map data
+	ft_free_map_struct(map);
 	return 0;
 }
 
@@ -45,7 +44,7 @@ int	ft_file_is_dot_ber(char *file)
 
 int	ft_init_map(t_map_data	*map, char *map_file)
 {
-	t_map_data	*map_copy;
+	t_map_data	map_copy;
 
 	// count map lines
 	map->height = ft_get_map_height(map_file);
@@ -73,19 +72,16 @@ int	ft_init_map(t_map_data	*map, char *map_file)
 	if (ft_validate_outer_walls(map))
 		return (ft_print_error("Invalid map: missing surrounding walls"), 1);
 	
-	map_copy = malloc(sizeof(t_map_data));
-	if (map_copy == NULL)
-		return (ft_print_error("Internal error checking map"), 1);
-	ft_memcpy(map_copy, map, sizeof(t_map_data));
-	
-	ft_flood_fill(map_copy, map_copy->player_row, map_copy->player_col);
-	if (map_copy->exit != 0 || map_copy->player != 0 || map_copy->collectionable != 0)
+	// make copy for flood fill
+	map_copy = *map;
+	ft_flood_fill(&map_copy, map_copy.player_row, map_copy.player_col);
+	if (map_copy.exit != 0 || map_copy.player != 0 || map_copy.collectionable != 0)
 	{
-		printf("error despues de flood fill - remains exit: %d player: %d collectionables: %d\n", map_copy->exit, map_copy->player, map_copy->collectionable);
-		ft_free_map_struct(map_copy);
+		printf("error despues de flood fill - remains exit: %d player: %d collectionables: %d\n", map_copy.exit, map_copy.player, map_copy.collectionable);
 		return (ft_print_error("Inaccesible exit or collectibles"), 1);
 	}
-	return (ft_free_map_struct(map_copy), 0);
+
+	return (0);
 }
 
 void ft_print_error(char *error_msg)
